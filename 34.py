9a592a7be49e1813b2753996187bfa58668962e7
bethamanoj <bethamanoj01@gmail.com>
2026-02-04 22:34:08 +0530

    Frequency of characters

--- 34.py
+++ 34.py
@@ -1,4 +1,10 @@
 ch = input()
-s = set(ch)
-for i in s:
-	print(i,'=',ch.count(i))
+s = ""
+for i in ch:
+	if i not in s:
+		c = 0
+		for j in ch:
+			if i == j:
+				c += 1
+		print(i,'=',c)
+		s = s+i
