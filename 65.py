9813343a018d7c078787ecbb6349cee9c8ff833f
bethamanoj <bethamanoj01@gmail.com>
2026-02-28 23:38:43 +0530

    sequence 2,5,9,14,...

--- 65.py
+++ 65.py
@@ -1,5 +1,8 @@
 n = int(input())
 k = 2
 for i in range(1,n+1):
-    print(k,end=",")
-    k = k + (i+1)+1
+    if n == i:
+        print(k,end=',...')
+    else:
+        print(k,end=",")
+        k = k + (i+1)+1
