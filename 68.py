30393430a1065acb4ea38e2f4d44a64268165709
bethamanoj <bethamanoj01@gmail.com>
2026-02-28 23:31:58 +0530

    sequence 2,6,18,54,...

--- 68.py
+++ 68.py
@@ -1,5 +1,8 @@
 n = int(input())
 k = 2
 for i in range(1,n+1):
-    print(k,end=",")
-    k = 2 * (3 ** i)
+    if n == i:
+        print(k)
+    else:
+        print(k,end=",")
+        k = 2 * (3 ** i)
