6bdd244a90451357f8099ef49153e60670dc05d2
bethamanoj <bethamanoj01@gmail.com>
2026-02-28 23:34:18 +0530

    sequence 8,13,19,26,...

--- 67.py
+++ 67.py
@@ -2,6 +2,9 @@ n = int(input())
 k = 8
 m = 5
 for i in range(1,n+1):
-    print(k,end=",")
-    k = k + m
-    m = m+1
+    if n == i:
+        print(k,end = ",...")
+    else:
+        print(k,end=",")
+        k = k + m
+        m = m+1
